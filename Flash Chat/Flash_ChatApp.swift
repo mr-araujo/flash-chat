f7730159d03b96272fefc422ce69d4314e44b7b5
Murillo R. Araujo <mr_araujo@icloud.com>
2021-06-16 18:16:34 -0300

    Initial Commit

---
+++ Flash Chat/Flash_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flash_ChatApp.swift
+//  Flash Chat
+//
+//  Created by Murillo R. Araujo on 16/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Flash_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
